25df5cf9e8a3ff7f3f7c24f329daaf37e8879499
Emmageo3 <kanfanyemma22@gmail.com>
2022-01-12 23:48:53 +0000

    half

--- script.js
+++ script.js
@@ -73,3 +73,19 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+/**----------------swiper slide------------------- */
+
+let swiper = new Swiper('.portfolio-container',{
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
